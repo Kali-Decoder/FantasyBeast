a6ea848d376574341b640452ad7812174ab5b7c3
sneha gupta <snehagupta98930@gmail.com>
2025-05-26 22:06:34 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const { getOrCreateUser } = require("./services/userServices");
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
